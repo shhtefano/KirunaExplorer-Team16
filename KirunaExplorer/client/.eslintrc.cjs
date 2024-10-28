5c1184aecb800c8a6a01f2775f9f085cd5bef6bf
Nicolai Faye <nicohfaye@gmail.com>
2024-10-28 13:19:25 +0100

    Added and set up Jest for unit testing client

--- KirunaExplorer/client/.eslintrc.cjs
+++ KirunaExplorer/client/.eslintrc.cjs
@@ -1,16 +1,28 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    jest: true, //
+  },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "plugin:jest/recommended", // Add Jest recommended rules (optional, requires eslint-plugin-jest)
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "18.2",
+    },
+  },
+  plugins: ["react-refresh", "jest"], // Add Jest plugin (optional)
   rules: {
     "react/jsx-no-target-blank": "off",
     "react-refresh/only-export-components": [
