009e11b50ab76a4decd7759a26bf4161c78f3f78
Nicolai Faye <nicohfaye@gmail.com>
2024-11-15 08:47:20 +0100

    Initial commit

--- KirunaExplorer/client/src/components/DrawMap.jsx
+++ KirunaExplorer/client/src/components/DrawMap.jsx
@@ -1,27 +1,36 @@
 import React, { useState, useEffect } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import L from "leaflet";
-import { MapContainer, TileLayer, FeatureGroup, Marker, Polygon } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  FeatureGroup,
+  Marker,
+  Polygon,
+} from "react-leaflet";
 import { EditControl } from "react-leaflet-draw";
 import osm from "./osm-providers";
 import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
 import API from "../services/API";
-import { Modal, Button } from "react-bootstrap"; 
+import { Modal, Button } from "react-bootstrap";
 
 // Configura l'icona di default di Leaflet
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
-  iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
-  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
 });
 
 const DrawMap = () => {
   const [center] = useState({ lat: 67.85572, lng: 20.22513 });
   const [mapLayers, setMapLayers] = useState([]);
-  const [selectedDocument, setSelectedDocument] = useState(null); 
-  const [showModal, setShowModal] = useState(false); 
+  const [selectedDocument, setSelectedDocument] = useState(null);
+  const [showModal, setShowModal] = useState(false);
   const ZOOM_LEVEL = 15;
 
   useEffect(() => {
@@ -32,9 +41,9 @@ const DrawMap = () => {
 
         const layers = documents.map((doc) => ({
           id: doc.document_id,
-          type: "marker", 
+          type: "marker",
           latlngs: [doc.lat, doc.long],
-          document: doc, 
+          document: doc,
         }));
 
         setMapLayers(layers);
@@ -47,8 +56,8 @@ const DrawMap = () => {
   }, []);
 
   const handleMarkerClick = (document) => {
-    setSelectedDocument(document); 
-    setShowModal(true); 
+    setSelectedDocument(document);
+    setShowModal(true);
   };
 
   const _onCreate = (e) => {
@@ -71,7 +80,11 @@ const DrawMap = () => {
   return (
     <div className="row">
       <div className="col text-center">
-        <MapContainer center={center} zoom={ZOOM_LEVEL} style={{ height: "700px", width: "1400px" }}>
+        <MapContainer
+          center={center}
+          zoom={ZOOM_LEVEL}
+          style={{ height: "700px", width: "1400px" }}
+        >
           <FeatureGroup>
             <EditControl
               position="topright"
@@ -92,13 +105,16 @@ const DrawMap = () => {
                   key={layer.id}
                   position={layer.latlngs}
                   eventHandlers={{
-                    click: () => handleMarkerClick(layer.document), 
+                    click: () => handleMarkerClick(layer.document),
                   }}
                 />
               )
             )}
           </FeatureGroup>
-          <TileLayer url={osm.maptiler.url} attribution={osm.maptiler.attribution} />
+          <TileLayer
+            url={osm.maptiler.url}
+            attribution={osm.maptiler.attribution}
+          />
         </MapContainer>
       </div>
 
@@ -108,14 +124,35 @@ const DrawMap = () => {
             <Modal.Title>Document info</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <p><strong>Name:</strong> {selectedDocument.document_title}</p>
-            <p><strong>Stakeholder:</strong> {selectedDocument.stakeholder}</p>
-            <p><strong>Scale:</strong> {selectedDocument.scale}</p>
-            <p><strong>Date:</strong> {selectedDocument.issuance_date}</p>
-            <p><strong>Language:</strong> {selectedDocument.language}</p>
-            <p><strong>Pages:</strong> {selectedDocument.pages}</p>
-            <p><strong>Document Type:</strong> {selectedDocument.document_type}</p>
-            <p><strong>Description:</strong> {selectedDocument.document_description}</p>
+            <p>
+              <strong>Name:</strong> {selectedDocument.document_title}
+            </p>
+            <p>
+              <strong>Stakeholder:</strong> {selectedDocument.stakeholder}
+            </p>
+            <p>
+              <strong>Scale:</strong> {selectedDocument.scale}
+            </p>
+            <p>
+              <strong>Date:</strong> {selectedDocument.issuance_date}
+            </p>
+            <p>
+              <strong>Language:</strong> {selectedDocument.language}
+            </p>
+            <p>
+              <strong>Pages:</strong> {selectedDocument.pages}
+            </p>
+            <p>
+              <strong>Document Type:</strong> {selectedDocument.document_type}
+            </p>
+            <p>
+              <strong>Coordinates:</strong> {selectedDocument.lat},{" "}
+              {selectedDocument.long}
+            </p>
+            <p>
+              <strong>Description:</strong>{" "}
+              {selectedDocument.document_description}
+            </p>
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={() => setShowModal(false)}>
