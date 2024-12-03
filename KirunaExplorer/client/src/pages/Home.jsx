c364539afa9d7eae6c4d6bc84a50dfbbae26d331
shhtefano <sfngln00@gmail.com>
2024-12-03 19:53:41 +0100

    [FIX] small ui fixes homepage

--- KirunaExplorer/client/src/pages/Home.jsx
+++ KirunaExplorer/client/src/pages/Home.jsx
@@ -71,7 +71,7 @@ const HomePage = () => {
             <div className="space-y-4 text-left mt-2">
               <div className="flex items-center justify-between">
                 <Button
-                style={{ minWidth: "200px" }}
+                  style={{ minWidth: "200px" }}
                   variant="outline"
                   className="flex items-center"
                   onClick={() => navigate("/documents/list")}
@@ -85,7 +85,7 @@ const HomePage = () => {
               </div>
               <div className="flex items-center justify-between">
                 <Button
-                                style={{ minWidth: "200px" }}
+                  style={{ minWidth: "200px" }}
 
                   variant="outline"
                   className="flex items-center"
@@ -99,12 +99,12 @@ const HomePage = () => {
                 </p>
               </div>
 
-              
+
               {user?.role === "urban_planner" && (
                 <>
                   <div className="flex items-center justify-between">
                     <Button
-                                    style={{ minWidth: "200px" }}
+                      style={{ minWidth: "200px" }}
 
                       variant="outline"
                       className="flex items-center"
@@ -121,7 +121,7 @@ const HomePage = () => {
                   </div>
                   <div className="flex items-center justify-between">
                     <Button
-                                    style={{ minWidth: "200px" }}
+                      style={{ minWidth: "200px" }}
 
                       variant="outline"
                       className="flex items-center"
@@ -136,7 +136,7 @@ const HomePage = () => {
                   </div>
                   <div className="flex items-center justify-between">
                     <Button
-                                    style={{ minWidth: "200px" }}
+                      style={{ minWidth: "200px" }}
 
                       variant="outline"
                       className="flex items-center"
@@ -151,7 +151,7 @@ const HomePage = () => {
                   </div>
                   <div className="flex items-center justify-between">
                     <Button
-                                    style={{ minWidth: "200px" }}
+                      style={{ minWidth: "200px" }}
 
                       variant="outline"
                       className="flex items-center"
@@ -167,27 +167,35 @@ const HomePage = () => {
                 </>
               )}
             </div>
-        {!user && <LoginForm />}
-        {user && <UserProfileCard user={user} />}
           </CardContent>
 
         </Card>
+        <div style={{ zIndex: 10, position: "relative", width: "800px" }}>
+          {!user && <div style={{ display: "flex", justifyContent: "center" }}>
+            <LoginForm />
+          </div>}
+          {user &&
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <UserProfileCard user={user} />
+            </div>
+          }
+        </div>
       </div>
 
-        <Snackbar
-          open={openSnackbar}
-          autoHideDuration={2000}
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={2000}
+        onClose={handleCloseSnackbar}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      >
+        <Alert
           onClose={handleCloseSnackbar}
-          anchorOrigin={{ vertical: "top", horizontal: "center" }}
+          severity="error"
+          sx={{ width: "100%" }}
         >
-          <Alert
-            onClose={handleCloseSnackbar}
-            severity="error"
-            sx={{ width: "100%" }}
-          >
-            {snackbarMsg}
-          </Alert>
-        </Snackbar>
+          {snackbarMsg}
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
