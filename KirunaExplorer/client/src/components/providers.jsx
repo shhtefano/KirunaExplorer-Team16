80a3f975b86b1fee58d3d091c6579c6f3bc07d7f
shhtefano <sfngln00@gmail.com>
2024-12-10 21:34:55 +0100

    [KE-150] Added cluster icons

--- KirunaExplorer/client/src/components/providers.jsx
+++ KirunaExplorer/client/src/components/providers.jsx
@@ -7,10 +7,24 @@ import {
 import { Separator } from "@/components/ui/separator";
 import { AppSidebar } from "@/components/app-sidebar";
 import { AuthProvider } from "@/contexts/AuthContext";
-
-// All providers for ui goes here
-
+import { useLocation } from "react-router-dom"; // Importa useLocation per ottenere l'URL corrente
 export function Providers({ children }) {
+  const location = useLocation();
+
+  // Mappa dei titoli in base agli URL
+  const pageTitles = {
+    "/": "Dashboard",
+    "/map": "Map",
+    "/areas": "Areas",
+    "/graph": "Graph",
+    "/add-document-description": "New Document",
+    "/documents/list": "Show Documents",
+    "/addResources": "Resources Management",
+  };
+
+  // Titolo corrente in base all'URL
+  const currentPageTitle = pageTitles[location.pathname] || "Dashboard";
+
   return (
     <>
       <AuthProvider>
@@ -21,6 +35,15 @@ export function Providers({ children }) {
               <div className="flex items-center gap-2 px-4">
                 <SidebarTrigger className="-ml-1" />
                 <Separator orientation="vertical" className="mr-2 h-4" />
+                {/* Inserisci qui il titolo della pagina */}
+                <span className="font-semibold text-lg">
+
+                <a href="/" className="">
+                <u>Home</u> 
+                </a>
+                { " >"}
+                </span>
+                <span className=" text-lg"> <strong><u>{currentPageTitle}</u> </strong></span>
               </div>
             </header>
             <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
--- KirunaExplorer/client/src/pages/AreaMap.jsx
+++ KirunaExplorer/client/src/pages/AreaMap.jsx
@@ -5,22 +5,18 @@ import DrawMap from "../components/DrawMap";
 const AreaMap = () => {
   return (
     <div className="flex-col h-full w-full items-center justify-center space-y-6">
-      <div className="d-flex justify-content-center" style={{ fontSize: "34px", fontWeight: "bold", marginBottom: '20px', paddingTop: '20px' }}>
-        <h1>
-          Area Map
-        </h1>
-      </div>
-      <div style={{marginBottom:'20px'}}>
+
+      <div style={{marginTop:'20px', marginLeft:'20px'}}>
         <div>
 
       <DrawMap/>
         </div>
-        <div className="flex items-center justify-center align-center" style={{marginTop:'90px'}}>
+        {/* <div className="flex items-center justify-center align-center" style={{marginTop:'90px'}}>
           <a href="/">
 
             <Button variant="outline">Back to Home</Button>
           </a>
-        </div>
+        </div> */}
       </div>
 
     </div>
