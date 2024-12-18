3df092374b49dbac8065a06fff30175a50c0e5c0
Nicolai Faye <nicohfaye@gmail.com>
2024-12-18 21:06:17 +0100

    Edited setup.sql

---
+++ KirunaExplorer/server/utils/areas.sql
@@ -0,0 +1,39 @@
+
+
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2787017822266,	67.8589686452163,	1,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2780151367188,	67.8582893476797,	2,	NULL,	'Adjusted d,evelopment plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2769422531128,	67.8578041230353,	3,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2753114700317,	67.857189490655,    4,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2742385864258,	67.8565101412781,	5,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2735090255737,	67.8557498935059,	6,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2735090255737,	67.8549410920676,	7,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2745819091797,	67.8537763687093,	8,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2758693695068,	67.852886610283,    9,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2767276763916,	67.8521262443632	,10,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2776288986206,	67.8516085342318	,11,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2779293060303,	67.8510746336219	,12,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2781009674072,	67.850653976048	    ,13,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2781867980957,	67.8501362332338	,14,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2771139144897,	67.8484858513083	,15,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2806329727173,	67.8482107763005	,16,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2836799621582,	67.8481622333154	,17,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2875852584839,	67.8482107763005	,18,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2938079833984,	67.8482916810513	,19,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.3030776977539,	67.8482593191847	,20,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.3094291687012,	67.8481139541549	,21,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.3132915496826,	67.8478874184196	,22,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.3156089782715,	67.847725605833	    ,23,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.3180122375488,	67.8479845054327	,24,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.3165531158447,	67.848469934437	    ,25,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.31569480896,	    67.8490200750961	,26,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.3145790100098,	67.8501850360131	,27,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.3129482269287,	67.85151172619	    ,28,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.3105449676514,	67.8526765626596	,29,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.3073692321777,	67.8537119240126	,30,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.3033351898193,	67.8548442978592	,31,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.3001594543457,	67.8556531026549	,32,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2957820892334,	67.8562030738914	,33,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2889156341553,	67.8573029774485	,34,	NULL,	'Adjusted development plan');
+INSERT INTO Geolocation (area_id, long, lat, n_order, sub_area_id, area_name) VALUES(2,	20.2844524383545,	67.8581440451589	,35,	NULL,	'Adjusted development plan');
+
+
