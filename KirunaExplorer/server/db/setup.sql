39d978bb1557fc9b26b3eb9dfbfa58171403baad
Davide Licitra s331360 <s331360@studenti.polito.it>
2024-12-18 11:24:09 +0100

    [EDIT] Removed Dummy documents and insert of real documents and resources

--- KirunaExplorer/server/db/setup.sql
+++ KirunaExplorer/server/db/setup.sql
@@ -1,4 +1,4 @@
--- SQLite
+-- Eliminazione tabelle esistenti
 DROP TABLE IF EXISTS Attachments;
 DROP TABLE IF EXISTS Connections;
 DROP TABLE IF EXISTS Geolocation;
@@ -10,7 +10,7 @@ DROP TABLE IF EXISTS Document_Stakeholder;
 DROP TABLE IF EXISTS DocumentTypes;
 DROP TABLE IF EXISTS Type;
 
-
+-- Creazione delle tabelle
 CREATE TABLE "Connections" (
 	"parent_id"	INTEGER,
 	"children_id"	INTEGER,
@@ -41,8 +41,7 @@ CREATE TABLE "Documents" (
 	"document_type"	VARCHAR(50) NOT NULL,
 	"document_description"	TEXT NOT NULL,
 	PRIMARY KEY("document_id" AUTOINCREMENT)
-	);
-
+);
 
 CREATE TABLE "Geolocation" (
 	"area_id"	INTEGER NOT NULL,
@@ -61,10 +60,9 @@ CREATE TABLE "Geolocation_Documents" (
 	FOREIGN KEY("document_id") REFERENCES "Documents"("document_id") ON DELETE CASCADE
 );
 
-
 CREATE TABLE "Stakeholders" (
 	"stakeholder_id"	INTEGER,
-	"stakeholder_name"	INTEGER NOT NULL,
+	"stakeholder_name"	TEXT NOT NULL,
 	PRIMARY KEY("stakeholder_id" AUTOINCREMENT)
 );
 
@@ -74,78 +72,35 @@ CREATE TABLE "Type" (
 	PRIMARY KEY("type_id" AUTOINCREMENT)
 );
 
-INSERT INTO Stakeholders(stakeholder_id, stakeholder_name) VALUES(1, 'LKAB');
-INSERT INTO Stakeholders(stakeholder_id, stakeholder_name) VALUES(2, 'Citizens');
-INSERT INTO Stakeholders(stakeholder_id, stakeholder_name) VALUES(3, 'Hospital');
-INSERT INTO Stakeholders(stakeholder_id, stakeholder_name) VALUES(4, 'Assurance');
+-- Popolamento tabelle
+INSERT INTO Stakeholders(stakeholder_name) VALUES ('Kiruna kommun/Residents');
+INSERT INTO Stakeholders(stakeholder_name) VALUES ('LKAB');
+INSERT INTO Stakeholders(stakeholder_name) VALUES ('Kiruna kommun');
+INSERT INTO Stakeholders(stakeholder_name) VALUES ('White Arkitekter');
 
+INSERT INTO Type (type_name) VALUES ('Informative document');
+INSERT INTO Type (type_name) VALUES ('Prescriptive document');
+INSERT INTO Type (type_name) VALUES ('Design document');
+INSERT INTO Type (type_name) VALUES ('Technical document');
+INSERT INTO Type (type_name) VALUES ('Material effect');
 
-INSERT INTO Documents (document_id, document_title, scale, issuance_date, language, pages, document_type, document_description) VALUES
-(1, 'Kiruna buildings', '1:100', '2012', 'Swedish', 10, 'Technical', 'description'),
-(2, 'Kiruna workshops', '1:1000', '2012/12/25', 'English', 10, 'Technical', 'description'),
-(3, 'LKAB Policy', '1:1000', '2012/12/25', 'English', 10, 'Technical', 'description'),
-(4, 'Municipal Assurance', '1:1000', '2012/12/25', 'English', 10, 'Technical', 'description'),
-(5, 'LKAB Assurance', '1:1000', '2012/12/25', 'English', 10, 'Technical', 'description'),
-(7, 'LKAB Policy 1', '1:100', '2012/12/05', 'Swedish', 2, 'Technical', 'TEEST'),
-(8, 'Adjusted development plan', '1:100', '2024/11/20', 'English', 0, 'Agreement', 'description'),
-(9, 'Miners Assurance', '1:100', '2024/11/03', 'Swedish', 5, 'Conflict', 'Miners Assurance'),
-(10, 'Agreements 2024', '1:100', '2012/12/05', 'English - Advanced', 5, 'Agreement', 'ciao'),
-(11, 'Church Consultation', '1:100', '2012/12/05', 'Swedish', 2, 'Consultation', 'prova'),
-(12, 'Inside Kiruna', '1:100', '2012/12/05', 'Swedish', 2, 'Prescriptive', 'test'),
-(13, 'Alpha Industries', 'Blueprint', '2020/12', 'English', 49, 'Material Effects', 'Test'),
-(14, 'Alpha Assurance 1', '1:100', '2020', 'Swedish', 5, 'Agreement', 'Alpha Assurance'),
-(15, 'Alpha Assurance 2', '1:100', '2020', 'Swedish', 5, 'Agreement', 'Alpha Assurance'),
-(16, 'Alpha Assurance 3', '1:100', '2020', 'Swedish', 5, 'Agreement', 'Alpha Assurance'),
-(17, 'Alpha Assurance 4', '1:100', '2020', 'Swedish', 5, 'Agreement', 'Alpha Assurance'),
-(18, 'Alpha Assurance 5', '1:100', '2020', 'Swedish', 5, 'Agreement', 'Alpha Assurance'),
-(19, 'Alpha Assurance 6', '1:100', '2020', 'Swedish', 5, 'Agreement', 'Alpha Assurance');
+INSERT INTO Documents (document_title, scale, issuance_date, language, pages, document_type, document_description) VALUES
+('Compilation of responses So what the people of Kiruna think?', 'Text', '2007-01-01', 'Swedish', NULL, 'Informative document', 'This document is a compilation of the responses to the survey "What is your impression of Kiruna?" From the citizens\' responses to this last part of the survey, it is evident that certain buildings, such as the Kiruna Church, the Hjalmar Lundbohmsgården, and the Town Hall, are considered of significant value to the population. The municipality views the experience of this survey positively, to the extent that over the years it will propose various consultation opportunities.');
 
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(1,1);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(2,1);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(2,2);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(2,3);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(1,4);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(1,5);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(1,6);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(1,7);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(1,8);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(1,9);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(1,10);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(1,11);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(1,12);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(1,13);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(1,14);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(1,15);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(1,16);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(1,17);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(1,18);
-INSERT INTO Document_Stakeholder(stakeholder_id, document_id) VALUES(1,19);
+INSERT INTO Documents (document_title, scale, issuance_date, language, pages, document_type, document_description) VALUES('Town Hall demolition (64)', 'blueprints/effects', '04/2019', '','', 'Material effect', 'After the construction of the new town hall wascompleted, the old building, nicknamed "The Igloo,"was demolished. The only elements preserved werethe door handles, a masterpiece of Sami art made ofwood and bone, and the clock tower, which oncestood on the roof of the old town hall. The clocktower was relocated to the central square of NewKiruna, in front of the new building.');
+
+INSERT INTO Documents (document_title, scale, issuance_date, language, pages, document_type, document_description) VALUES
+('Development Plan (41)', '1 : 7,500', '2014-03-17', 'Swedish', 111, 'Design document', 'The development plan shapes the form of the new city. The document, unlike previous competition documents, is written entirely in Swedish, which reflects the target audience: the citizens of Kiruna. The plan obviously contains many elements of the winning masterplan from the competition, some recommended by the jury, and others that were deemed appropriate to integrate later. The document is divided into four parts, with the third part, spanning 80 pages, describing the shape the new city will take and the strategies to be implemented for its relocation through plans, sections, images, diagrams, and texts. The document also includes numerous studies aimed at demonstrating the future success of the project.');
+
+INSERT INTO Documents (document_title, scale, issuance_date, language, pages, document_type, document_description) VALUES
+('Deformation forecast (45)', '1 : 12,000', '2014-12-01', 'Swedish', 1, 'Technical document', 'The deformation forecast predicts the development of deformations affecting the urban area of Kiruna. The report includes visual aids and detailed technical descriptions of geological impacts and the measures to counteract them.');
+
+-- Associazioni documenti e stakeholder
+INSERT INTO Document_Stakeholder (stakeholder_id, document_id) VALUES (1, 1);
+INSERT INTO Document_Stakeholder (stakeholder_id, document_id) VALUES (2, 2);
+INSERT INTO Document_Stakeholder (stakeholder_id, document_id) VALUES (3, 3);
+INSERT INTO Document_Stakeholder (stakeholder_id, document_id) VALUES (2, 4);
 
 INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 1);
 INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 2);
 INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 3);
-INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 4);
-INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 5);
-INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 6);
-INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 7);
-INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 8);
-INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 9);
-INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 10);
-INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 11);
-INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 12);
-INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 13);
-INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 14);
-INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 15);
-INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 16);
-INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 17);
-INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 18);
-INSERT INTO Geolocation_Documents(area_id, document_id) VALUES(1, 19);
-
-INSERT INTO Type (type_name) VALUES ('Design');
-INSERT INTO Type (type_name) VALUES ('Informative');
-INSERT INTO Type (type_name) VALUES ('Technical');
-INSERT INTO Type (type_name) VALUES ('Prescriptive');
-INSERT INTO Type (type_name) VALUES ('Material Effects');
-INSERT INTO Type (type_name) VALUES ('Agreement');
-INSERT INTO Type (type_name) VALUES ('Conflict');
-INSERT INTO Type (type_name) VALUES ('Consultation');
